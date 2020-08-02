d8a72f465de6b4310623e9bc51db79168d2409c2
Joseph <jsazon427@gmail.com>
2020-08-02 18:04:51 +0800

    feat: server - login user functionality

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,5 +1,46 @@
-const loginUser = async (req, res, next) => {
-  res.send('login user from service');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../models/User');
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials.' });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {
+        expiresIn: 3600,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 };
 
 module.exports = {
