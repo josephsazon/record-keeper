80d98e7193daa6a4241797d05cbad6bd8462ffad
Joseph <jsazon427@gmail.com>
2020-08-03 22:26:08 +0800

    feat: server - refactor user service and controller in request handling

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -18,7 +18,16 @@ router.post(
     check('password', 'Password is required.').exists(),
   ],
   validate,
-  userService.createUser
+  async (req, res) => {
+    try {
+      const user = await userService.createUser(req.body);
+
+      res.status(200).json({ user });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: err.message });
+    }
+  }
 );
 
 /**
@@ -26,6 +35,15 @@ router.post(
  * @description   Get logged in user.
  * @access        Private
  */
-router.get('/', auth, userService.getUser);
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await userService.getUser(req.user);
+
+    res.status(200).json({ user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: err.message });
+  }
+});
 
 module.exports = router;
--- server/services/user.js
+++ server/services/user.js
@@ -2,43 +2,31 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
-const createUser = async (req, res) => {
-  const { username, password } = req.body;
+const createUser = async ({ username, password }) => {
+  let user = await User.findOne({ username });
 
-  try {
-    let user = await User.findOne({ username });
-
-    if (user) {
-      return res.status(400).json({ msg: 'User already exists.' });
-    }
-
-    user = new User({
-      username,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-
-    user.password = await bcrypt.hash(password, salt);
-
-    await user.save();
-
-    res.json({ user });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+  if (user) {
+    throw new Error('User already exists.');
   }
+
+  user = new User({
+    username,
+    password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  user.password = await bcrypt.hash(password, salt);
+
+  await user.save();
+
+  return user;
 };
 
-const getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
+const getUser = async ({ id }) => {
+  const user = await User.findById(id).select('-password');
 
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
+  return user;
 };
 
 module.exports = {
