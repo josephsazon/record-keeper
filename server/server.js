487ea34554266748c0269b841e66900bd7e5458c
Joseph <jsazon427@gmail.com>
2020-09-06 12:15:59 +0800

    chore - prepare deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const routes = require('./routes');
 
@@ -13,6 +14,16 @@ app.use(express.json({ extended: false }));
 
 // Define route.
 routes.init(app);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '/../client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '/../client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
